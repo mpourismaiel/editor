cbe07c190e0d87fced14b51415441a5fb75f1da8
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2018-01-25 23:41:52 +0330

    feat: Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  parser: 'typescript',
+}
