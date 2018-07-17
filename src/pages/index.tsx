26a052d5b8904da819f9182ddd61b0eba353ce04
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2018-07-17 14:56:16 +0430

    chore: Eject and add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import './styles.scss';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
